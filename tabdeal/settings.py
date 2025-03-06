adbbff3f15864dccddbdc762d1f284eea7b0da46
Parsa <parsadeveloper.py@gmail.com>
2025-03-06 23:40:08 +0330

    last

--- tabdeal/settings.py
+++ tabdeal/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
